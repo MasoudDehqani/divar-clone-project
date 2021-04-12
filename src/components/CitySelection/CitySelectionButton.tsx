da41f6d00ce51008f233375f1cbe2843d209f738
Masoud <masoud67.md@gmail.com>
2021-04-12 23:58:32 +0430

    all cities added to modal

--- src/components/CitySelection/CitySelectionButton.tsx
+++ src/components/CitySelection/CitySelectionButton.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import { Button, styled } from '@material-ui/core'
-import Link from "@material-ui/core/Link"
-import { NavLink, useRouteMatch } from "react-router-dom"
+import React from "react";
+import { Button, styled } from "@material-ui/core";
+import Link from "@material-ui/core/Link";
+import { NavLink, useRouteMatch } from "react-router-dom";
 
 interface CityButtonPropsType {
-  isActive: boolean
+  isActive: boolean;
 }
 
 const CityButton = styled(Button)({
-  color: ({isActive} : CityButtonPropsType) => isActive ? "white" : "#be3737",
+  color: ({ isActive }: CityButtonPropsType) => isActive ? "white" : "#a62626",
   border: "1px solid rgba(166,38,38,.56)",
   width: "140px",
   height: "40px",
   fontFamily: "Vazir",
   fontWeight: "bold",
-  backgroundColor: ({isActive}) => isActive ? "#be3737" : ""
-})
+  backgroundColor: ({ isActive }) => (isActive ? "#a62626" : ""),
+});
 
 interface PropsType {
   text: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
-  to: string
+  to: string;
 }
 
-const CitySelectionButton = ({to, text, onClick} : PropsType) => {
-
-  const isActive = useRouteMatch<{city: string}>("/:city")?.params.city === to.substr(1)
+const CitySelectionButton = ({ to, text, onClick }: PropsType) => {
+  const isActive =
+    useRouteMatch<{ city: string }>("/:city")?.params.city === to.substr(1);
 
   return (
     <Link component={NavLink} to={to} underline="none">
@@ -33,10 +33,10 @@ const CitySelectionButton = ({to, text, onClick} : PropsType) => {
         {text}
       </CityButton>
     </Link>
-  )
-}
+  );
+};
 
-export default CitySelectionButton
+export default CitySelectionButton;
 
 // #a12727
 // #be3737
--- src/components/CitySelection/CitySelectionModal.tsx
+++ src/components/CitySelection/CitySelectionModal.tsx
@@ -1,107 +1,138 @@
-import React from 'react'
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-import { Box, Grid, InputAdornment, TextField } from '@material-ui/core';
-import { Close, Search } from '@material-ui/icons';
-import { allCities } from "../Sidebar/dataStructured"
-import CitySelectionButton from '../CitySelection/CitySelectionButton';
+import React, { useState } from "react";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import { Box, Grid, InputAdornment, TextField } from "@material-ui/core";
+import { Close, Search } from "@material-ui/icons";
+import { allCities } from "../Sidebar/dataStructured";
+import CitySelectionButton from "../CitySelection/CitySelectionButton";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     modal: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      overflow: "scroll"
+      overflow: "scroll",
     },
     paper: {
       backgroundColor: theme.palette.background.paper,
-      boxShadow: theme.shadows[5],
       padding: theme.spacing(2),
-      height: "fit-content",
-      width: "60%",
+      width: "52%",
+      margin: "1rem auto",
+      borderRadius: "4px",
+      outline: "none"
     },
     searchInput: {
       width: "100%",
       fontFamily: "Vazir",
-      marginBottom: "40px"
-    }
-  }),
+      marginBottom: "40px",
+    },
+  })
 );
 
 interface PropsType {
   modalOpen: boolean;
-  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const CitySelectionModal = ({ modalOpen, setModalOpen } : PropsType) => {
+const CitySelectionModal = ({ modalOpen, setModalOpen }: PropsType) => {
+
+  const [textFieldValue, setTextFieldValue] = useState("")
 
   const classes = useStyles();
+
+  const textFieldValueHandle = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setTextFieldValue(e.target.value)
+  }
 
   const handleClose = () => {
     setModalOpen(false);
   };
 
   const handleCitySelection = (cityUrl: string) => {
-    localStorage.setItem("city", cityUrl)
-  }
+    localStorage.setItem("city", cityUrl);
+  };
 
   return (
-      <Modal
-        className={classes.modal}
-        open={modalOpen}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 5,
-        }}
-      >
-        <Fade in={modalOpen}>
-          <div className={classes.paper}>
+    <Modal
+      className={classes.modal}
+      open={modalOpen}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <div className={classes.paper}>
+        <Box display="flex" justifyContent="space-between" mb="60px">
+          <span style={{ fontWeight: "bold", fontSize: "1.3rem" }}>
+            انتخاب شهر
+          </span>
+          <Close
+            style={{ cursor: "pointer" }}
+            onClick={() => setModalOpen(false)}
+          />
+        </Box>
 
-            <Box display="flex" justifyContent="space-between" mb="60px">
-              <span style={{fontWeight: "bold", fontSize: "1.3rem"}}>انتخاب شهر</span>
-              <Close style={{cursor: "pointer"}} onClick={() => setModalOpen(false)} />
-            </Box>
+        <TextField
+          value={textFieldValue}
+          onChange={textFieldValueHandle}
+          className={classes.searchInput}
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            ),
+          }}
+        />
 
-            <TextField
-              className={classes.searchInput}
-              variant="outlined"
-              id="input-with-icon-textfield"
-              InputProps={{
-              endAdornment: (
-                <InputAdornment position="start">
-                  <Search />
-                </InputAdornment>
-              ),
-              }}
-            />
+        {!textFieldValue &&
+        <>
+          <h3>شهرهای پر بازدید</h3>
+          <Grid container spacing={3}>
+            {allCities.topCities.map(({ title, url }) => {
+              if (title.includes(textFieldValue) || url.includes(textFieldValue)) {
+                return (
+                  <Grid key={url} item onClick={() => handleCitySelection(url)}>
+                    <CitySelectionButton
+                      to={`/${url}`}
+                      onClick={handleClose}
+                      text={title}
+                    />
+                  </Grid>
+                )
+              }
+              return undefined  
+            }
+            )}
+          </Grid>
+        </>
+        }
 
-            <h3>شهرهای پر بازدید</h3>
-            <Grid container spacing={2}>
-              {allCities.topCities.map(({title, url}) => 
-                <Grid key={url} item onClick={() => handleCitySelection(url)}>
-                  <CitySelectionButton to={`/${url}`} onClick={handleClose} text={title} />  
-                </Grid>
-              )}
-
-            </Grid>
-
-            <h3>همه شهرها</h3>
-            <Grid container spacing={2}>
-              {allCities.compressedData.map( ([id, cityName, cityUrl] : (number | string | any)[]) =>
-                <Grid key={id} item>
-                  <CitySelectionButton text={cityName} to={`/${cityUrl}`} onClick={handleClose} />
-                </Grid>
-              )}
-            </Grid>
-          </div>
-        </Fade>
-      </Modal>
+        {!textFieldValue && <h3>همه شهرها</h3>}
+          <Grid container spacing={3}>
+            {allCities.compressedData.map(
+              ([id, cityName, cityUrl]: (number | string | any)[]) => {
+                if (cityName.includes(textFieldValue) || cityUrl.includes(textFieldValue)) {
+                  return (
+                    <Grid key={id} item>
+                      <CitySelectionButton
+                        text={cityName}
+                        to={`/${cityUrl}`}
+                        onClick={handleClose}
+                      />
+                    </Grid>
+                  )  
+                }
+                return undefined
+              }
+            )}
+          </Grid>
+      </div>
+    </Modal>
   );
-}
+};
 
-export default CitySelectionModal
+export default CitySelectionModal;
